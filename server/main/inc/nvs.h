e4853e535d6d5c6996ce1ed8f11f591eda35a207
Rogério Júnior <junior.rogerio8@gmail.com>
2021-11-04 02:57:46 -0300

    And God said, “Let there be light,” and there was light.
    Co-authored-by: Ana Carolina <ancarolcs@gmail.com>

---
+++ server/main/inc/nvs.h
@@ -0,0 +1,6 @@
+#ifndef NVS_H
+#define NVS_H
+
+void start_nvs();
+
+#endif
---
+++ server/main/src/main.c
@@ -0,0 +1,53 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+#include "dht11.h"
+#include "wifi.h"
+#include "mqtt.h"
+#include "nvs.h"
+
+xSemaphoreHandle mutex_wifi_connection;
+xSemaphoreHandle conexaoMQTTSemaphore;
+
+void start_MQTT(void *params)
+{
+    while (true)
+    {
+        if (xSemaphoreTake(mutex_wifi_connection, portMAX_DELAY))
+        {
+            ESP_LOGI("MQTT", "Wi-fi conected");
+            mqtt_start();
+        }
+    }
+}
+
+void send_messages(void *params)
+{
+    if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
+    {
+        while (true)
+        {
+            mqtt_envia_mensagem("Roger/teste/fse", "Eita nóis!");
+            vTaskDelay(5000 / portTICK_PERIOD_MS);
+        }
+    }
+}
+
+void app_main()
+{
+    start_nvs();
+
+    mutex_wifi_connection = xSemaphoreCreateBinary();
+    conexaoMQTTSemaphore = xSemaphoreCreateBinary();
+
+    // DHT11_init(GPIO_NUM_4);
+
+    wifi_start();
+
+    xTaskCreate(&start_MQTT, "MQTT start", 2048, NULL, 1, NULL);
+    xTaskCreate(&send_messages, "Send messages", 2048, NULL, 1, NULL);
+}
